10d0a226c1f79e470d17665ee5df239ce84395aa
tushartajane <krishagni@Tushars-Macbook.local>
2019-02-07 09:11:07 +0530

    This is the Linux script for problem statement 1: Accept the file and and the number of lines and disply the nummber of lines present in the file

---
+++ Prob1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Accept a file name and a number (x). Display x lines from the top of the file.
+#Check if the file exists and is readable. The value of x should not exceed the total number of lines in the files. Display suitable messages in case an error is encountered.
+
+num='';
+echo "Enter the  number of line you want to display";
+read num;
+echo "enter the file name"
+read fname;
+line_no=$(awk '{x++} END {print x}' que1)
+echo "Input No: $num";
+echo "File Count: $line_no";
+if [ $num -le $line_no ]
+then
+  head -$num que1
+else
+  echo "The num,ber of line present in the file is less than of your input: No of lines in the file: $line_no ";
+fi
